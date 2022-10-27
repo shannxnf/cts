8f089c5f63a8844291618865f132a1a28675fda9
Shannon <shannonferns05@gmail.com>
2022-10-27 16:29:18 +0400

    commit 2

--- hello.java
+++ hello.java
@@ -1,8 +1,7 @@
 public class hello {
-
-public static void main (String[] args) {
+ public static void main (String[] args) {
  
-system.out.println("Hello Java");
+System.out.println("Hello Java");
 
 }
 
